337e17213dedbffe65e837d541fb64e3a8f587dc
Yao Min <ngyaominsg@gmail.com>
2020-12-09 00:20:46 +0800

    Add donation link

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from '../routes';
 
-export default () => {
+
+//export default () => {
+export default class ThisComponent extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      copySuccess: false
+    }
+  }
+
+  copyCodeToClipboard = () => {
+    const el = this.textArea
+    el.select()
+    document.execCommand("copy")
+    this.setState({copySuccess: true})
+  }
+
+  render() {
   return (
     <Menu style={{ marginTop: '10px' }}>
       <Link route="https://ethereum.org/en/">
@@ -10,12 +28,25 @@ export default () => {
           <i class="ethereum icon"></i>
           </a>
       </Link>
-      <img class="item" src="https://icon-icons.com/icons2/1873/PNG/32/dibujo-34_119874.png"></img>
+      <Link route="/">
+        <a className="item">
+          <img src="https://icon-icons.com/icons2/1873/PNG/32/dibujo-34_119874.png"></img>
+        </a>
+      </Link>
       <Link route="/">
         <a className="item">
           <b>Safety Pin</b>, a Decentralize Peace Of Mind Platform
         </a>
       </Link>
+      <button className="item" onClick={() => this.copyCodeToClipboard()}>
+        <b>Donate Eths to >>> Click here to Copy</b>
+      </button>
+      <div className="item">
+        <textarea
+        ref={(textarea) => this.textArea = textarea}
+        value="0xa85A05887Ae1f8f4Ce2Ce102c20d4aAFD1273981"
+        />
+      </div>
         <Menu.Menu position="right">
           <Link route="/">
             <a className="item">Insurance Pools</a>
@@ -27,3 +58,4 @@ export default () => {
     </Menu>
   );
 };
+}
